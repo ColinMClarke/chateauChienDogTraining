0262a7a9fc52c49f378c46e7250f4bb126b8980e
COLIN CLARKE <colinmclarke@outlook.com>
2024-03-24 14:16:09 -0400

    testing welcome alert

--- js/welcome.js
+++ js/welcome.js
@@ -1,7 +1,11 @@
 /* This script creates a welcome message upon loading of the homepage. */
-<script>
+
 function welcomeMessage() {
-  alert("Hello! I am an alert box!")};
-</script>
+  alert("Hello! I am an alert box!");
+}
+
+  
+
+
 
   
